3cb44bea375366a07d6062cea96f56dfed24a6d2
Israel Philippe <israelphilippe304@gmail.com>
2023-11-20 03:31:21 -0300

    first test route configured in router.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 const express = require('express')
+const router = require('./router')
 
-const app = express();
+const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).send('Welcome hello')
-})
+app.use(router)
 
 module.exports = app
